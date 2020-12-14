c1b3d8a0bc0d71acdd45cda366d0c5e8c4cc4f1e
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2020-12-13 20:27:51 -0500

    Updates

---
+++ precache-manifest.5029b2f7067fdf07d95a3a7c51355ca9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d573a88be74a27b502d45db8f62dbfe8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c487ab04cd68dd417301",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c218a950d8d743b71a83",
+    "url": "./static/js/2.bcb452f4.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "./static/js/2.bcb452f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c487ab04cd68dd417301",
+    "url": "./static/js/main.488609e4.chunk.js"
+  },
+  {
+    "revision": "ab5009f13b89e9771be6",
+    "url": "./static/js/runtime-main.d5f440ca.js"
+  }
+]);
